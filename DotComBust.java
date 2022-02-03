bba89d63199cca08270c901cfe1a66038f04c83c
Viktor Naftaev <viktornaftaev@gmail.com>
2022-02-03 20:25:47 +0700

    полностью готовый класс DotComBust

--- DotComBust.java
+++ DotComBust.java
@@ -17,6 +17,49 @@ public class DotComBust {
         dotComList.add(obj2);
         dotComList.add(obj3);
 
+        System.out.println("Ваша цель - потопить три сайта.");
+        System.out.println("pet.com, rest.eu, ssuwt.ru");
+        System.out.println("Попытайся потопить их за минимальное количество ходов");
 
+        for (DotCom dotComToSet : dotComList) {
+            ArrayList<String> newLocation = helper.placeDotCom(3);
+            dotComToSet.setLocationCells(newLocation);
+        }
+    }
+    private void startPlaying() {
+        while(!dotComList.isEmpty()){
+            String userGuess = helper.getUserInput("Сделайте ход");
+            checkUserGuess(userGuess);
+        }
+        finishGame();
+    }
+    private void checkUserGuess(String userGuess){
+        numOfGuesses++;
+        String result = "Мимо";
+        for(DotCom dotComToTest : dotComList){
+            result = dotComToTest.checkYourself(userGuess);
+            if (result.equals("Попал")){
+                break
+            }
+            if (result.equals("Потопил")){
+                dotComList.remove(dotComToTest);
+            }
+        }
+        System.out.println(result);
+    }
+
+    private void finishGame(){
+        System.out.println("Конец игры");
+        if(numOfGuesses < 10){
+            System.out.println("Вы молодец!");
+        } else {
+            System.out.println("Улучшайте свой результат");
+        }
+    }
+
+    public static void main(String[] args){
+        DotComBust game = new DotComBust();
+        game.setUpGame();
+        game.startPlaying();
     }
 }
